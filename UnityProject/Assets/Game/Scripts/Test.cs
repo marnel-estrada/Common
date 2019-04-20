782c224da3ccd19709ac05d456e276efe243ddc8
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2019-04-20 18:33:36 +0800

    Moved inside UnityProject

--- UnityProject/Assets/Game/Scripts/Test.cs
+++ UnityProject/Assets/Game/Scripts/Test.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Common;
 
 using UnityEngine;
 
 public class Test : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
+        SampleScript script = new SampleScript();
+        script.Call();
     }
 
     // Update is called once per frame

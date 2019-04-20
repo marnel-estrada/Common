cd47d2495564099d629051d9633548ee491674cb
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2019-04-20 14:12:40 +0800

    Sample class

--- Assets/SampleCommon/Feature/SampleScript.cs
+++ Assets/SampleCommon/Feature/SampleScript.cs
@@ -1,18 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class SampleScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+namespace Common {
+    public class SampleScript {
+        public void Call() {
+            Debug.Log("SampleScript");
+        }
     }
 }

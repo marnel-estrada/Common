ba1899cb113f0b4d4a2df0ff124fc451de01ede5
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2019-08-27 00:33:06 +0800

    SignalHandlerWithCustomProcessedTypeSystem

--- UnityProject/Assets/CommonEcs/Scripts/Signal/DestroySignalsSystem.cs
+++ UnityProject/Assets/CommonEcs/Scripts/Signal/DestroySignalsSystem.cs
@@ -4,11 +4,11 @@ namespace CommonEcs {
     [UpdateInGroup(typeof(PresentationSystemGroup))]
     public class DestroySignalsSystem : ComponentSystem {
         private EntityQuery query;
-    
+
         protected override void OnCreate() {
             this.query = GetEntityQuery(typeof(Signal), typeof(SignalFramePassed));
         }
-    
+
         protected override void OnUpdate() {
             this.EntityManager.DestroyEntity(this.query);
         }

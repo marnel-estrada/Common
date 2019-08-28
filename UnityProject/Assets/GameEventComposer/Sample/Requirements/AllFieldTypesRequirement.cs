7128be4c4dcd96b6a9784e1d0edb03991a0d6485
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2019-08-28 15:33:11 +0800

    Assembly definition files

---
+++ UnityProject/Assets/GameEventComposer/Sample/Requirements/AllFieldTypesRequirement.cs
@@ -0,0 +1,15 @@
+using Common;
+
+namespace GameEvent {
+    [Group("GameEvent.Sample")]
+    public class AllFieldTypesRequirement : Requirement {
+        public NamedString text { get; set; }
+        public NamedInt integer { get; set; }
+        public NamedFloat floatingNumber { get; set; }
+        public NamedBool boolean { get; set; }
+
+        public override bool IsMet() {
+            return false;
+        }
+    }
+}
---
+++ UnityProject/Assets/GameEventComposer/Scripts/Rarity.cs
@@ -0,0 +1,39 @@
+using Common;
+
+namespace GameEvent {
+    // Enum implemented as a struct
+    public readonly struct Rarity {
+        public static readonly Rarity NULL = new Rarity(0, "Null");
+        public static readonly Rarity COMMON = new Rarity(1, "Common");
+        public static readonly Rarity UNCOMMON = new Rarity(2, "Uncommon");
+        public static readonly Rarity RARE = new Rarity(3, "Rare");
+
+        public static readonly Rarity[] ALL = {
+            COMMON, UNCOMMON, RARE
+        };
+
+        /// <summary>
+        /// Convert from string
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static Rarity Convert(string name) {
+            for (int i = 0; i < ALL.Length; ++i) {
+                if (ALL[i].Equals(name)) {
+                    return ALL[i];
+                }
+            }
+
+            Assertion.Assert(false, $"Can't convert rarity text {name}");
+            return NULL;
+        }
+        
+        public readonly int id;
+        public readonly string name;
+
+        private Rarity(int id, string name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+}

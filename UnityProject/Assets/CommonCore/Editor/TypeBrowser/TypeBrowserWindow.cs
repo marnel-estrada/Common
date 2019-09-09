e4caea786ab637ba599a8052f2580e80fbc50718
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2019-09-09 15:25:13 +0800

    TypeBrowserWindow

---
+++ UnityProject/Assets/CommonCore/Editor/TypeBrowser/TypeBrowserWindow.cs
@@ -0,0 +1,85 @@
+using System;
+
+using UnityEditor;
+
+using UnityEngine;
+
+namespace Common {
+    /// <summary>
+    /// A generic browser window that can support any base type
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class TypeBrowserWindow<T> : EditorWindow {
+        private Action<Type> onAdd;
+
+        private Type selectedType;
+        private GUISkin skin;
+
+        private TypeSelectionRenderer typeSelection;
+
+        private string baseTypeName;
+        
+        /// <summary>
+        ///     Initializer
+        /// </summary>
+        /// <param name="onAdd"></param>
+        public void Init(GUISkin skin, Action<Type> onAdd) {
+            this.skin = skin;
+            this.onAdd = onAdd;
+            
+            Assertion.AssertNotNull(this.skin);
+
+            this.typeSelection =
+                new TypeSelectionRenderer(typeof(T), this.skin.customStyles[0], OnSelectionChange);
+
+            this.baseTypeName = typeof(T).Name;
+        }
+
+        private void OnSelectionChange(Type type) {
+            // Set to selected type
+            this.selectedType = type;
+        }
+
+        private void OnGUI() {
+            EditorGUILayout.BeginVertical();
+
+            GUILayout.Label($"{this.baseTypeName} Browser", EditorStyles.boldLabel);
+            GUILayout.Space(10);
+
+            this.typeSelection.Render();
+
+            GUILayout.FlexibleSpace();
+
+            GUILayout.Space(5);
+
+            GUILayout.Label("Selected: " + (this.selectedType == null ? "(none)" : this.selectedType.Name));
+            if (GUILayout.Button($"Add {this.baseTypeName}", GUILayout.Width(120))) {
+                AddSelectedType();
+            }
+
+            EditorGUILayout.EndVertical();
+        }
+
+        private void AddSelectedType() {
+            if (this.selectedType == null) {
+                EditorUtility.DisplayDialog($"Add {this.baseTypeName}", $"No selected {this.baseTypeName}", "OK");
+                return;
+            }
+
+            Close(); // close the window
+
+            this.onAdd(this.selectedType);
+        }
+
+        private void Update() {
+            // close the window if editor is compiling
+            if (EditorApplication.isCompiling) {
+                Close();
+            }
+        }
+
+        private void OnLostFocus() {
+            Close();
+        }
+    }
+}

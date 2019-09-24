4566ee5801973de437aa5a2a0e8cb956ca9489f9
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2019-09-24 19:22:33 +0800

    Added Icon, Text, and TextColor

--- UnityProject/Assets/GameEvent/Scripts/Effect.cs
+++ UnityProject/Assets/GameEvent/Scripts/Effect.cs
@@ -1,8 +1,16 @@
+using UnityEngine;
+
 namespace GameEvent {
     public abstract class Effect {
         /// <summary>
         /// Applies the effect
         /// </summary>
         public abstract void Apply();
+
+        public virtual string Icon { get; }
+
+        public virtual string Text { get; }
+        
+        public virtual Color TextColor { get; }
     }
 }

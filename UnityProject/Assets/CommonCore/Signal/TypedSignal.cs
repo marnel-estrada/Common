6e79e7c0c02ef33b7bfb92ab0b3a62c76b66481e
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2020-02-24 15:13:19 +0800

    Cleaned code

--- UnityProject/Assets/CommonCore/Signal/TypedSignal.cs
+++ UnityProject/Assets/CommonCore/Signal/TypedSignal.cs
@@ -2,9 +2,12 @@
     /// <summary>
     /// Another signal implementation that uses typed parameters
     /// This is to avoid boxing
+    ///
+    /// We only limit parameters to struct so that users of this wouldn't pass references or
+    /// create small objects from classes which is garbage.
     /// </summary>
-    public class TypedSignal<TParameter> where TParameter : struct {
-        public delegate void SignalListener(TParameter parameter);
+    public class TypedSignal<T> where T : struct {
+        public delegate void SignalListener(T parameter);
         
         private readonly SimpleList<SignalListener> listeners = new SimpleList<SignalListener>(1);
 
@@ -20,7 +23,7 @@
         /// <summary>
         /// Invokes all listeners to the signal
         /// </summary>
-        public void Dispatch(TParameter parameter) {
+        public void Dispatch(T parameter) {
             int listenersCount = this.listeners.Count;
             for (int i = 0; i < listenersCount; ++i) {
                 this.listeners[i](parameter); // Invoke the delegate

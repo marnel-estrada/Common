2160f756b8d524a6b8ee0714440d6ef64c56fdf8
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2020-04-13 18:03:05 +0800

    Used struct matcher for equality

--- UnityProject/Assets/CommonCore/OptionEqualityMatcher.cs
+++ UnityProject/Assets/CommonCore/OptionEqualityMatcher.cs
@@ -1,39 +1,18 @@
 namespace Common {
-    public class OptionEqualityMatcher<T> : IFuncOptionMatcher<T, bool> where T : class {
-        private Option<T> other;
+    public readonly struct OptionEqualityMatcher<T> : IFuncOptionMatcher<T, bool> where T : class {
+        private readonly Option<T> other;
 
-        public void Init(Option<T> other) {
+        public OptionEqualityMatcher(Option<T> other) {
             this.other = other;
         }
 
-        private readonly CompareMatcher<T> compareMatcher = new CompareMatcher<T>();
-
         public bool OnSome(T value) {
-            // Match the value for other
-            this.compareMatcher.Init(value);
-            return this.other.Match(this.compareMatcher);
+            return this.other.ReferenceEquals(value);
         }
 
         public bool OnNone() {
             // Return true if the other is none as well
             return this.other.IsNone;
-        }
-
-        private class CompareMatcher<T> : IFuncOptionMatcher<T, bool> where T : class {
-            private T otherValueToCompare;
-
-            public void Init(T otherValueToCompare) {
-                this.otherValueToCompare = otherValueToCompare;
-            }
-
-            public bool OnSome(T value) {
-                return value == this.otherValueToCompare;
-            }
-
-            public bool OnNone() {
-                // Return true if the other specified value is null (none as well)
-                return this.otherValueToCompare == null;
-            }
         }
     }
 }

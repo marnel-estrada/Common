3af8ddcbfa3d8b81fa944ebb6efaa8b5940db0b2
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2020-04-14 01:08:15 +0800

    Used SortJob for ComputeBufferSprite

--- UnityProject/Assets/CommonEcs.ComputeBufferSprite/Components/SortEntry.cs
+++ UnityProject/Assets/CommonEcs.ComputeBufferSprite/Components/SortEntry.cs
@@ -1,5 +1,7 @@
+using System;
+
 namespace CommonEcs {
-    public struct SortedEntry {
+    public struct SortEntry : IComparable<SortEntry> {
         public int index; // The index of the sprite to the master list in its draw instance
         public int layerOrder;
         public float renderOrder;
@@ -9,10 +11,33 @@ namespace CommonEcs {
         /// </summary>
         /// <param name="index"></param>
         /// <param name="renderOrder"></param>
-        public SortedEntry(int index, int layerOrder, float renderOrder) {
+        public SortEntry(int index, int layerOrder, float renderOrder) {
             this.index = index;
             this.layerOrder = layerOrder;
             this.renderOrder = renderOrder;
+        }
+
+        public int CompareTo(SortEntry other) {
+            if (this.layerOrder < other.layerOrder) {
+                return -1;
+            }
+
+            if (this.layerOrder > other.layerOrder) {
+                return 1;
+            }
+
+            // At this point, they have the same layerOrder
+            // We check the renderOrder
+            if (this.renderOrder < other.renderOrder) {
+                return -1;
+            }
+
+            if (this.renderOrder > other.renderOrder) {
+                return 1;
+            }
+
+            // They are equal
+            return 0;
         }
     }
 }
--- UnityProject/Assets/CommonEcs.ComputeBufferSprite/UpdateDrawInstanceArraysSystem.cs
+++ UnityProject/Assets/CommonEcs.ComputeBufferSprite/UpdateDrawInstanceArraysSystem.cs
@@ -39,18 +39,16 @@ namespace CommonEcs {
                 }
                 
                 NativeArray<ComputeBufferSprite> masterList = drawInstance.SpritesMasterList;
-                NativeArray<SortedEntry> sortEntries = new NativeArray<SortedEntry>(masterList.Length, Allocator.TempJob);
+                NativeArray<SortEntry> sortEntries = new NativeArray<SortEntry>(masterList.Length, Allocator.TempJob);
                 
-                // Reset
-                lastHandle = new ResetSortEntriesJob() {
+                // Populate sortEntries
+                lastHandle = new PopulateJob() {
                     spriteMasterList = masterList,
                     sortEntries = sortEntries
                 }.Schedule(masterList.Length, 64, lastHandle);
                 
-                // Sort
-                lastHandle = new SortJob() {
-                    sortEntries = sortEntries
-                }.Schedule(lastHandle);
+                // Sort (SortJob is fast!)
+                lastHandle = sortEntries.SortJob(lastHandle);
                 
                 // Update arrays
                 // sortEntries will be deallocated on this job
@@ -69,88 +67,17 @@ namespace CommonEcs {
         }
 
         [BurstCompile]
-        private struct ResetSortEntriesJob : IJobParallelFor {
+        private struct PopulateJob : IJobParallelFor {
             [ReadOnly]
             [NativeDisableParallelForRestriction]
             public NativeArray<ComputeBufferSprite> spriteMasterList;
 
             [NativeDisableParallelForRestriction]
-            public NativeArray<SortedEntry> sortEntries;
+            public NativeArray<SortEntry> sortEntries;
 
             public void Execute(int index) {
                 ComputeBufferSprite sprite = this.spriteMasterList[index];
-                this.sortEntries[index] = new SortedEntry(index, sprite.layerOrder, sprite.renderOrder); 
-            }
-        }
-        
-        [BurstCompile]
-        private struct SortJob : IJob {
-            public NativeArray<SortedEntry> sortEntries;
-
-            public void Execute() {
-                if (this.sortEntries.Length > 0) {
-                    Quicksort(0, this.sortEntries.Length - 1);
-                }
-            }
-
-            private void Quicksort(int left, int right) {
-                int i = left;
-                int j = right;
-                SortedEntry pivot = this.sortEntries[(left + right) / 2];
-
-                while (i <= j) {
-                    // Lesser
-                    while (Compare(this.sortEntries[i], ref pivot) < 0) {
-                        ++i;
-                    }
-
-                    // Greater
-                    while (Compare(this.sortEntries[j], ref pivot) > 0) {
-                        --j;
-                    }
-
-                    if (i <= j) {
-                        // Swap
-                        SortedEntry temp = this.sortEntries[i];
-                        this.sortEntries[i] = this.sortEntries[j];
-                        this.sortEntries[j] = temp;
-
-                        ++i;
-                        --j;
-                    }
-                }
-
-                // Recurse
-                if (left < j) {
-                    Quicksort(left, j);
-                }
-
-                if (i < right) {
-                    Quicksort(i, right);
-                }
-            }
-
-            private int Compare(SortedEntry a, ref SortedEntry b) {
-                if (a.layerOrder < b.layerOrder) {
-                    return -1;
-                }
-
-                if (a.layerOrder > b.layerOrder) {
-                    return 1;
-                }
-
-                // At this point, they have the same layerOrder
-                // We check the renderOrder
-                if (a.renderOrder < b.renderOrder) {
-                    return -1;
-                }
-
-                if (a.renderOrder > b.renderOrder) {
-                    return 1;
-                }
-
-                // They are equal
-                return 0;
+                this.sortEntries[index] = new SortEntry(index, sprite.layerOrder, sprite.renderOrder); 
             }
         }
         
@@ -163,7 +90,7 @@ namespace CommonEcs {
             [ReadOnly]
             [NativeDisableParallelForRestriction]
             [DeallocateOnJobCompletion]
-            public NativeArray<SortedEntry> sortEntries;
+            public NativeArray<SortEntry> sortEntries;
             
             public NativeArray<float4> transforms;
             public NativeArray<float> rotations;

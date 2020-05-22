361af6d1c05a01f3789eb29f82be1daedb6e6ea8
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2020-05-23 00:15:35 +0800

    Removed UVs array

--- UnityProject/Assets/CommonCore/TexturePacker/PackedTextureEntry.cs
+++ UnityProject/Assets/CommonCore/TexturePacker/PackedTextureEntry.cs
@@ -8,8 +8,6 @@ namespace Common {
         private readonly Rect uvRect;
         private readonly Rect spriteRect;
 
-        private readonly Vector2[] uvs = new Vector2[4];
-
         // Note that the passed uvRect may already be a scaled image
         // We keep a copy of the original dimension so we can still use the sprite as though it has
         // used its original size
@@ -25,19 +23,13 @@ namespace Common {
             this.atlas = atlas;
             this.uvRect = uvRect;
 
-            // Prepare UVs
-            this.uvs[0] = this.uvRect.min;
-            this.uvs[1].Set(this.uvRect.xMax, this.uvRect.yMin);
-            this.uvs[2].Set(this.uvRect.xMin, this.uvRect.yMax);
-            this.uvs[3] = this.uvRect.max;
-
             this.spriteRect = new Rect(this.uvRect.x * this.atlas.width, this.uvRect.y * this.atlas.height, 
                 this.uvRect.width * this.atlas.width, this.uvRect.height * this.atlas.height);
 
             this.originalWidth = originalWidth;
             this.originalHeight = originalHeight;
         }
-
+        
         public Texture2D Atlas {
             get {
                 return atlas;
@@ -56,12 +48,6 @@ namespace Common {
             }
         }
 
-        public Vector2[] Uvs {
-            get {
-                return this.uvs;
-            }
-        }
-
         public int OriginalWidth {
             get {
                 return this.originalWidth;
@@ -76,14 +62,13 @@ namespace Common {
 
         public float2 LowerLeftUv {
             get {
-                return new float2(this.uvs[0].x, this.uvs[0].y);
+                return this.uvRect.min;
             }
         }
 
-        public float2 UvDimension {
+        public float2 UvSize {
             get {
-                Vector2 difference = this.uvs[3] - this.uvs[0];
-                return new float2(difference.x, difference.y);
+                return this.uvRect.size;
             }
         }
     }

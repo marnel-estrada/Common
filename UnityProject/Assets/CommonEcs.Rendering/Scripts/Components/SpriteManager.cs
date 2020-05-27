115ec1a30545ec097a82fce3d0df8daecfc8ffd9
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2020-05-27 23:57:30 +0800

    Removed SpriteManger.SortList

--- UnityProject/Assets/CommonEcs.Rendering/Scripts/Components/SpriteManager.cs
+++ UnityProject/Assets/CommonEcs.Rendering/Scripts/Components/SpriteManager.cs
@@ -106,12 +106,6 @@ namespace CommonEcs {
             }
         }
 
-        public NativeList<SortedSpriteEntry> SortList {
-            get {
-                return this.internalInstance.sortList;
-            }
-        }
-
         public NativeArray<int> NativeTriangles {
             get {
                 return this.internalInstance.nativeTriangles;
@@ -312,7 +306,6 @@ namespace CommonEcs {
         /// </summary>
         public void Dispose() {
             this.internalInstance.nativeVertices.Dispose();
-            this.internalInstance.sortList.Dispose();
             this.internalInstance.nativeTriangles.Dispose();
             this.internalInstance.nativeUv.Dispose();
             this.internalInstance.nativeUv2.Dispose();
@@ -347,8 +340,6 @@ namespace CommonEcs {
 
             private int capacity;
             public int spriteCount;
-
-            public NativeList<SortedSpriteEntry> sortList;
 
             public int layer;
 
@@ -418,8 +409,6 @@ namespace CommonEcs {
                 this.nativeTriangles = new NativeArray<int>(trianglesLength, Allocator.Persistent);
 
                 this.mesh = new Mesh();
-
-                this.sortList = new NativeList<SortedSpriteEntry>(allocationCount, Allocator.Persistent);
 
                 this.spriteCount = 0;
             }

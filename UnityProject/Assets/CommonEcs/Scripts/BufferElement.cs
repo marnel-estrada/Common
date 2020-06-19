d9f8c0d02bf2bcc62df6f7db7411e5b7939699c4
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2020-06-19 18:00:56 +0800

    Changed capacity to 16

--- UnityProject/Assets/CommonEcs/Scripts/BufferElement.cs
+++ UnityProject/Assets/CommonEcs/Scripts/BufferElement.cs
@@ -4,7 +4,7 @@ namespace CommonEcs {
     /// <summary>
     /// A generic IBufferElement that can wrap any struct
     /// </summary>
-    [InternalBufferCapacity(10)]
+    [InternalBufferCapacity(16)]
     public readonly struct BufferElement<T> : IBufferElementData where T : struct {
         public readonly T value;
     

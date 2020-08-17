42ae7427a08a9ec2b19dfac46bec60a2326a4a76
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2020-08-17 23:20:56 +0800

    Renamed parentType to ownerType

--- UnityProject/Assets/CommonCore/Reflection/StaticFieldsInvoker.cs
+++ UnityProject/Assets/CommonCore/Reflection/StaticFieldsInvoker.cs
@@ -1,20 +1,18 @@
 using System;
 using System.Reflection;
 
-using UnityEngine;
-
 namespace Common {
     public class StaticFieldsInvoker {
-        private readonly Type parentType;
+        private readonly Type ownerType;
         private readonly FieldInfo[] fields;
         
         // The methods to call
         private readonly SimpleList<string> methodNames = new SimpleList<string>(1);
 
         // This ensures that there's always at least one method name
-        public StaticFieldsInvoker(Type parentType, string methodName) {
-            this.parentType = parentType;
-            this.fields = this.parentType.GetFields(BindingFlags.Public | BindingFlags.Static);
+        public StaticFieldsInvoker(Type ownerType, string methodName) {
+            this.ownerType = ownerType;
+            this.fields = this.ownerType.GetFields(BindingFlags.Public | BindingFlags.Static);
             
             AddMethod(methodName);
         }
@@ -35,9 +33,11 @@ namespace Common {
         private static readonly object[] EMPTY_PARAMETERS = new object[0];
 
         private void Execute(string methodName) {
-            foreach (FieldInfo field in this.fields) {
-                // The type of the field does not matter. As long it has the method
-                // ClearProvider(), it will be invoked
+            for (int i = 0; i < this.fields.Length; ++i) {
+                FieldInfo field = this.fields[i];
+                
+                // The type of the field does not matter. As long it has the named method,
+                // it will be invoked
                 object instance = field.GetValue(null);
                 MethodInfo clearMethod = field.FieldType.GetMethod(methodName);
                 if (clearMethod != null) {

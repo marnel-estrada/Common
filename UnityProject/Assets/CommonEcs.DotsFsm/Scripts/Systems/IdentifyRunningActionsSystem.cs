5cf2573c9926a7df0c242246f37f5668ea6ef9b3
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2021-02-23 23:55:53 +0800

    Turned to custom job so it's faster

--- UnityProject/Assets/CommonEcs.DotsFsm/Scripts/Systems/IdentifyRunningActionsSystem.cs
+++ UnityProject/Assets/CommonEcs.DotsFsm/Scripts/Systems/IdentifyRunningActionsSystem.cs
@@ -1,18 +1,45 @@
+using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 
 namespace CommonEcs.DotsFsm {
     [UpdateAfter(typeof(ConsumePendingEventSystem))]
     public class IdentifyRunningActionsSystem : SystemBase {
-        protected override void OnUpdate() {
-            this.Entities.ForEach(delegate(ref DotsFsmAction action) {
-                DotsFsmState state = GetComponent<DotsFsmState>(action.stateOwner);
-                DotsFsm fsm = GetComponent<DotsFsm>(state.fsmOwner);
+        private EntityQuery query;
+        
+        protected override void OnCreate() {
+            this.query = GetEntityQuery(typeof(DotsFsmAction));
+        }
 
-                // Action can run if the FSM's current state is the action's state owner and
-                // the fsm has no pending event
-                // We no longer run the action if the FSM has a pending event
-                action.running = fsm.currentState.ValueOr(default) == action.stateOwner && fsm.pendingEvent.IsNone;
-            }).ScheduleParallel();
+        protected override void OnUpdate() {
+            Job job = new Job() {
+                actionType = GetComponentTypeHandle<DotsFsmAction>(), allFsms = GetComponentDataFromEntity<DotsFsm>()
+            };
+
+            this.Dependency = job.ScheduleParallel(this.query, 1, this.Dependency);
+        }
+        
+        [BurstCompile]
+        private struct Job : IJobEntityBatch {
+            public ComponentTypeHandle<DotsFsmAction> actionType;
+
+            [ReadOnly]
+            public ComponentDataFromEntity<DotsFsm> allFsms;
+            
+            public void Execute(ArchetypeChunk batchInChunk, int batchIndex) {
+                NativeArray<DotsFsmAction> actions = batchInChunk.GetNativeArray(this.actionType);
+                for (int i = 0; i < actions.Length; ++i) {
+                    DotsFsmAction action = actions[i];
+                    DotsFsm fsm = this.allFsms[action.fsmOwner];
+                    
+                    // Action can run if the FSM's current state is the action's state owner and
+                    // the fsm has no pending event
+                    // We no longer run the action if the FSM has a pending event
+                    action.running = fsm.currentState.ValueOr(default) == action.stateOwner && fsm.pendingEvent.IsNone;
+
+                    actions[i] = action; // Modify
+                }
+            }
         }
     }
 }

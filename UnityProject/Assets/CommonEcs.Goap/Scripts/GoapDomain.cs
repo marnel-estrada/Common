908a13b643e56b4a66b71ae781c4c773c359d0c8
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2021-03-14 20:28:16 +0800

    Removed BlobArray

--- UnityProject/Assets/CommonEcs.Goap/Scripts/GoapDomain.cs
+++ UnityProject/Assets/CommonEcs.Goap/Scripts/GoapDomain.cs
@@ -1,13 +1,36 @@
 using Unity.Collections;
-using Unity.Entities;
 
 namespace CommonEcs.Goap {
     public struct GoapDomain {
         // This is the list of actions
-        public BlobArray<GoapPlanningAction> actions;
+        public FixedList4096<GoapPlanningAction> actions;
         
         // The list of integers mapped to the condition are indices to actions
         public FixedHashMap<Condition, FixedList32<int>> actionMap;
+
+        public void AddAction(in GoapPlanningAction action) {
+            int actionIndex = this.actions.Length;
+            this.actions.Add(action);
+            
+            // Add to action map
+            FixedList32<int> indexList = ResolveFixedList(action.effect);
+            indexList.Add(actionIndex);
+            
+            // Update map
+            this.actionMap.AddOrSet(action.effect, indexList);
+        }
+
+        private FixedList32<int> ResolveFixedList(in Condition effect) {
+            ValueTypeOption<FixedList32<int>> found = this.actionMap.Find(effect);
+            if (found.IsSome) {
+                return found.ValueOr(default);
+            }
+            
+            // Not found yet. We create a new list.
+            FixedList32<int> newList = new FixedList32<int>();
+            this.actionMap.AddOrSet(effect, newList);
+            return newList;
+        }
 
         public GoapPlanningAction GetAction(int index) {
             return this.actions[index];

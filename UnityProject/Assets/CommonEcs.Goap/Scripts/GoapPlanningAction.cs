7cbdc9e2cafb339ccacde9cabf4acc6ab425050b
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2021-03-17 22:15:13 +0800

    Implement IEquatable

--- UnityProject/Assets/CommonEcs.Goap/Scripts/GoapPlanningAction.cs
+++ UnityProject/Assets/CommonEcs.Goap/Scripts/GoapPlanningAction.cs
@@ -1,9 +1,11 @@
+using System;
+
 namespace CommonEcs.Goap {
     /// <summary>
     /// This is an action that is used in planning. This is not the actual action that
     /// will be executed.
     /// </summary>
-    public struct GoapPlanningAction {
+    public struct GoapPlanningAction : IEquatable<GoapPlanningAction> {
         public ConditionList10 preconditions;
         public readonly Condition effect;
         
@@ -20,6 +22,26 @@ namespace CommonEcs.Goap {
 
         public void AddPrecondition(Condition condition) {
             this.preconditions.Add(condition);
+        }
+
+        public bool Equals(GoapPlanningAction other) {
+            return this.id == other.id;
+        }
+
+        public override bool Equals(object obj) {
+            return obj is GoapPlanningAction other && Equals(other);
+        }
+
+        public override int GetHashCode() {
+            return this.id;
+        }
+
+        public static bool operator ==(GoapPlanningAction left, GoapPlanningAction right) {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(GoapPlanningAction left, GoapPlanningAction right) {
+            return !left.Equals(right);
         }
     }
 }

32f286786477b8d316db9f851d62a9f5410dc5fb
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2021-04-10 16:25:40 +0800

    Added updateCount

--- UnityProject/Assets/CommonEcs/Scripts/Systems/ScalableTimeSystemGroup.cs
+++ UnityProject/Assets/CommonEcs/Scripts/Systems/ScalableTimeSystemGroup.cs
@@ -1,3 +1,5 @@
+using Common;
+
 using Unity.Entities;
 
 namespace CommonEcs {
@@ -5,5 +7,23 @@ namespace CommonEcs {
     /// A system group which can be controlled how many times it runs per frame 
     /// </summary>
     public class ScalableTimeSystemGroup : ComponentSystemGroup {
+        private int updateCount = 1;
+
+        public int UpdateCount {
+            get {
+                return this.updateCount;
+            }
+            
+            set {
+                this.updateCount = value;
+                Assertion.IsTrue(this.updateCount > 0);
+            }
+        }
+
+        protected override void OnUpdate() {
+            for (int i = 0; i < this.updateCount; ++i) {
+                base.OnUpdate();
+            }
+        }
     }
 }

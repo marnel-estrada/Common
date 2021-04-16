87558beb90522f5e2e292973cb166ea3da42ed4d
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2021-04-16 19:52:12 +0800

    Removed preconditions

--- UnityProject/Assets/GoapBrain/Scripts/EditorData/GoapExtensionData.cs
+++ UnityProject/Assets/GoapBrain/Scripts/EditorData/GoapExtensionData.cs
@@ -1,32 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace GoapBrain {
     [Serializable]
     public class GoapExtensionData {
-
-        [SerializeField]
-        private List<ConditionData> preconditions = new List<ConditionData>();
-
         [SerializeField]
         private GoapDomainData domainData;
 
-        public GoapDomainData DomainData {
+        public GoapDomainData? DomainData {
             get {
-                return domainData;
+                return this.domainData;
             }
 
             set {
                 this.domainData = value;
             }
         }
-
-        public List<ConditionData> Preconditions {
-            get {
-                return preconditions;
-            }
-        }
-
     }
 }

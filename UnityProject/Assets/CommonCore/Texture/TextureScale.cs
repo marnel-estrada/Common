9c0983fa6efe2d73da9476cfae21da4efef45f6f
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2021-04-21 17:59:14 +0800

    Readded TextureScale (removed when fixing submodules)

--- UnityProject/Assets/CommonCore/Texture/TextureScale.cs
+++ UnityProject/Assets/CommonCore/Texture/TextureScale.cs
@@ -5,24 +5,15 @@ using UnityEngine;
 namespace Common {
     // Only works on ARGB32, RGB24 and Alpha8 textures that are marked readable
     public class TextureScale {
-        public class ThreadData {
-            public int start;
-            public int end;
 
-            public ThreadData(int s, int e) {
-                this.start = s;
-                this.end = e;
-            }
-        }
-
-        private static Color[] texColors;
-        private static Color[] newColors;
+        private static Color[]? texColors;
+        private static Color[]? newColors;
         private static int w;
         private static float ratioX;
         private static float ratioY;
         private static int w2;
         private static int finishCount;
-        private static Mutex mutex;
+        private static Mutex? mutex;
 
         public static void Point(Texture2D tex, int newWidth, int newHeight) {
             ThreadedScale(tex, newWidth, newHeight, false);
@@ -39,14 +30,14 @@ namespace Common {
                 ratioX = 1.0f / ((float) newWidth / (tex.width - 1));
                 ratioY = 1.0f / ((float) newHeight / (tex.height - 1));
             } else {
-                ratioX = ((float) tex.width) / newWidth;
-                ratioY = ((float) tex.height) / newHeight;
+                ratioX = (float) tex.width / newWidth;
+                ratioY = (float) tex.height / newHeight;
             }
 
             w = tex.width;
             w2 = newWidth;
-            var cores = Mathf.Min(SystemInfo.processorCount, newHeight);
-            var slice = newHeight / cores;
+            int cores = Mathf.Min(SystemInfo.processorCount, newHeight);
+            int slice = newHeight / cores;
 
             finishCount = 0;
             if (mutex == null) {
@@ -58,7 +49,7 @@ namespace Common {
                 ThreadData threadData;
                 for (i = 0; i < cores - 1; i++) {
                     threadData = new ThreadData(slice * i, slice * (i + 1));
-                    ParameterizedThreadStart ts = useBilinear ? new ParameterizedThreadStart(BilinearScale) :
+                    ParameterizedThreadStart ts = useBilinear ? BilinearScale :
                         new ParameterizedThreadStart(PointScale);
                     Thread thread = new Thread(ts);
                     thread.Start(threadData);
@@ -91,17 +82,17 @@ namespace Common {
             newColors = null;
         }
 
-        public static void BilinearScale(System.Object obj) {
+        public static void BilinearScale(object obj) {
             ThreadData threadData = (ThreadData) obj;
-            for (var y = threadData.start; y < threadData.end; y++) {
+            for (int y = threadData.start; y < threadData.end; y++) {
                 int yFloor = (int) Mathf.Floor(y * ratioY);
-                var y1 = yFloor * w;
-                var y2 = (yFloor + 1) * w;
-                var yw = y * w2;
+                int y1 = yFloor * w;
+                int y2 = (yFloor + 1) * w;
+                int yw = y * w2;
 
-                for (var x = 0; x < w2; x++) {
+                for (int x = 0; x < w2; x++) {
                     int xFloor = (int) Mathf.Floor(x * ratioX);
-                    var xLerp = x * ratioX - xFloor;
+                    float xLerp = x * ratioX - xFloor;
                     newColors[yw + x] =
                         ColorLerpUnclamped(
                             ColorLerpUnclamped(texColors[y1 + xFloor], texColors[y1 + xFloor + 1], xLerp),
@@ -115,12 +106,12 @@ namespace Common {
             mutex.ReleaseMutex();
         }
 
-        public static void PointScale(System.Object obj) {
+        public static void PointScale(object obj) {
             ThreadData threadData = (ThreadData) obj;
-            for (var y = threadData.start; y < threadData.end; y++) {
-                var thisY = (int) (ratioY * y) * w;
-                var yw = y * w2;
-                for (var x = 0; x < w2; x++) {
+            for (int y = threadData.start; y < threadData.end; y++) {
+                int thisY = (int) (ratioY * y) * w;
+                int yw = y * w2;
+                for (int x = 0; x < w2; x++) {
                     newColors[yw + x] = texColors[(int) (thisY + ratioX * x)];
                 }
             }
@@ -134,6 +125,15 @@ namespace Common {
             return new Color(c1.r + (c2.r - c1.r) * value, c1.g + (c2.g - c1.g) * value, c1.b + (c2.b - c1.b) * value,
                 c1.a + (c2.a - c1.a) * value);
         }
-        
+
+        public class ThreadData {
+            public int end;
+            public int start;
+
+            public ThreadData(int s, int e) {
+                this.start = s;
+                this.end = e;
+            }
+        }
     }
 }

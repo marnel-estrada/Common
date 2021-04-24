b2b09ae49534dcb120edef026bbdf21e8a3671e5
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2021-04-24 22:28:14 +0800

    Added ConstructQuery() for SystemBase

--- UnityProject/Assets/CommonEcs/Scripts/ComponentSystemBaseExtensions.cs
+++ UnityProject/Assets/CommonEcs/Scripts/ComponentSystemBaseExtensions.cs
@@ -35,5 +35,37 @@
                 All = all ?? Array.Empty<ComponentType>()
             };
         }
+        
+        /// <summary>
+        /// A shortcut for constructing an EntityArchetypeQuery
+        /// </summary>
+        /// <param name="system"></param>
+        /// <param name="any"></param>
+        /// <param name="none"></param>
+        /// <param name="all"></param>
+        /// <returns></returns>
+        public static EntityQueryDesc ConstructQuery(this SystemBase system, ComponentType[]? any, ComponentType[]? none, ComponentType[] all) {
+            return new EntityQueryDesc() {
+                Any = any ?? Array.Empty<ComponentType>(),
+                None = none ?? Array.Empty<ComponentType>(),
+                All = all ?? Array.Empty<ComponentType>()
+            };
+        }
+
+        /// <summary>
+        /// A shortcut for constructing an EntityArchetypeQuery
+        /// Accepts only All and None
+        /// </summary>
+        /// <param name="system"></param>
+        /// <param name="all"></param>
+        /// <param name="none"></param>
+        /// <returns></returns>
+        public static EntityQueryDesc ConstructQuery(this SystemBase system, ComponentType[] all, ComponentType[]? none) {
+            return new EntityQueryDesc() {
+                Any = Array.Empty<ComponentType>(),
+                None = none ?? Array.Empty<ComponentType>(),
+                All = all ?? Array.Empty<ComponentType>()
+            };
+        }
     }
 }

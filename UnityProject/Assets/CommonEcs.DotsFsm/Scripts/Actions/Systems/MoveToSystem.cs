0e01338216d29a1a1cd310ca4145e4b7a732f124
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2021-04-25 17:08:19 +0800

    Removed usage of DurationTimer for MoveToSystem

--- UnityProject/Assets/CommonEcs.DotsFsm/Scripts/Actions/Systems/MoveToSystem.cs
+++ UnityProject/Assets/CommonEcs.DotsFsm/Scripts/Actions/Systems/MoveToSystem.cs
@@ -1,170 +1,87 @@
-using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
-using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
 
 namespace CommonEcs.DotsFsm {
-    [UpdateInGroup(typeof(DotsFsmSystemGroup))]
-    [UpdateAfter(typeof(StartFsmSystem))]
-    [UpdateAfter(typeof(ConsumePendingEventSystem))]
-    [UpdateAfter(typeof(IdentifyRunningActionsSystem))]
-    public class MoveToSystem : JobSystemBase {
-        private EntityQuery query;
+    public class MoveToSystem : DotsFsmActionSystem<MoveTo, MoveToSystem.Execution> {
         private DotsFsmSystemGroup dotsFsmSystemGroup;
 
         protected override void OnCreate() {
-            this.query = GetEntityQuery(typeof(DotsFsmAction), typeof(MoveTo), typeof(DurationTimer));
+            base.OnCreate();
             this.dotsFsmSystemGroup = this.World.GetOrCreateSystem<DotsFsmSystemGroup>();
         }
-        
-        protected override JobHandle OnUpdate(JobHandle inputDeps) {
-            // Do the action first
-            ComponentTypeHandle<DotsFsmAction> actionHandle = GetComponentTypeHandle<DotsFsmAction>();
-            ComponentTypeHandle<MoveTo> moveToHandle = GetComponentTypeHandle<MoveTo>();
+
+        public struct Execution : IFsmActionExecution<MoveTo> {
+            [NativeDisableParallelForRestriction]
+            public ComponentDataFromEntity<Translation> allTranslation;
             
-            ActionJob actionJob = new ActionJob {
-                fsmActionHandle = actionHandle,
-                moveToHandle = moveToHandle,
-                timerHandle = GetComponentTypeHandle<DurationTimer>(),
-                rerunGroup = this.dotsFsmSystemGroup.RerunGroup
-            };
-
-            JobHandle handle = actionJob.ScheduleParallel(this.query, 1, inputDeps);
-
-            // Apply the position stored in MoveTo.currentPosition (denormalized)
-            ApplyToTranslations applyJob = new ApplyToTranslations {
-                allTranslations = GetComponentDataFromEntity<Translation>(),
-                actionHandle = actionHandle,
-                moveToHandle = moveToHandle
-            };
-
-            return applyJob.ScheduleParallel(this.query, 1, handle);
-        }
-        
-        [BurstCompile]
-        private struct ActionJob : IJobEntityBatch {
-            public ComponentTypeHandle<DotsFsmAction> fsmActionHandle;
-            public ComponentTypeHandle<MoveTo> moveToHandle;
-            public ComponentTypeHandle<DurationTimer> timerHandle;
-
             [NativeDisableParallelForRestriction]
             public NativeReference<bool> rerunGroup;
             
-            public void Execute(ArchetypeChunk batchInChunk, int batchIndex) {
-                NativeArray<DotsFsmAction> fsmActions = batchInChunk.GetNativeArray(this.fsmActionHandle);
-                NativeArray<MoveTo> moveTos = batchInChunk.GetNativeArray(this.moveToHandle);
-                NativeArray<DurationTimer> timers = batchInChunk.GetNativeArray(this.timerHandle);
-
-                for (int i = 0; i < batchInChunk.Count; ++i) {
-                    DotsFsmAction fsmAction = fsmActions[i];
-                    MoveTo moveTo = moveTos[i];
-                    DurationTimer timer = timers[i];
-                    
-                    Process(ref fsmAction, ref moveTo, ref timer);
-                    
-                    // Modify
-                    fsmActions[i] = fsmAction;
-                    moveTos[i] = moveTo;
-                    timers[i] = timer;
-                }
-            }
+            public float deltaTime;
             
-            private void Process(ref DotsFsmAction fsmAction, ref MoveTo moveTo, ref DurationTimer timer) {
-                if (fsmAction.running) {
-                    if (!fsmAction.entered) {
-                        OnEnter(ref fsmAction, ref moveTo, ref timer);
-                        fsmAction.entered = true;
-                        fsmAction.exited = false;
-                    }
-
-                    OnUpdate(ref fsmAction, ref moveTo, ref timer);
-                } else {
-                    if (fsmAction.entered && !fsmAction.exited) {
-                        // This means the action's state is no longer the FSM's current state
-                        // However, the state entered and hasn't exited yet
-                        // We're supposed to do OnExit() but the action doesn't need one 
-                        fsmAction.exited = true;
-                    }
-                    
-                    // Reset the states
-                    fsmAction.entered = false;
-                }
-            }
-
-            private void OnEnter(ref DotsFsmAction action, ref MoveTo moveTo,
-                ref DurationTimer timer) {
+            public void OnEnter(Entity actionEntity, ref DotsFsmAction action, ref MoveTo move) {
                 // Set to start position
-                moveTo.currentPosition = moveTo.start;
-
-                if (Comparison.IsZero(moveTo.duration)) {
+                this.allTranslation[move.targetEntity] = new Translation() {
+                    Value = move.start
+                };
+                
+                // Note here that the timer was already reset when MoveTo.Init() was invoked 
+                if (Comparison.IsZero(move.timer.duration)) {
                     // Duration is zero
                     // Let's finish right away
-                    Finish(ref action, ref moveTo);
-                    return;
+                    Finish(ref action, ref move);
                 }
-                
-                // Initialize timer
-                timer.Reset(moveTo.duration);
             }
-            
-            private void OnUpdate(ref DotsFsmAction action, ref MoveTo moveTo,
-                ref DurationTimer timer) {
-                if (timer.HasElapsed) {
+
+            public void OnUpdate(Entity actionEntity, ref DotsFsmAction action, ref MoveTo move) {
+                move.timer.Update(this.deltaTime);
+                
+                if (move.timer.HasElapsed) {
                     // Duration is done. Snap to destination.
-                    Finish(ref action, ref moveTo);
+                    Finish(ref action, ref move);
                     return;
                 }
                 
                 // Timer is not done yet
                 // Let's interpolate
-                float3 newPosition = math.lerp(moveTo.start, moveTo.destination, timer.Ratio);
-                moveTo.currentPosition = newPosition;
+                float3 newPosition = math.lerp(move.start, move.destination, move.timer.Ratio);
+                this.allTranslation[move.targetEntity] = new Translation() {
+                    Value = newPosition
+                };
+            }
+
+            public void OnExit(Entity actionEntity, DotsFsmAction action, ref MoveTo move) {
             }
             
             private void Finish(ref DotsFsmAction action, ref MoveTo moveTo) {
                 // Snap to destination
-                moveTo.currentPosition = moveTo.destination;
+                this.allTranslation[moveTo.targetEntity] = new Translation() {
+                    Value = moveTo.destination
+                };
                 
-                // Send event if it exists
-                if (moveTo.finishEvent.Length > 0) {
-                    action.SendEvent(moveTo.finishEvent);
-                    
-                    
+                // Send finish event if it exists
+                if (moveTo.finishEvent.IsSome) {
+                    action.SendEvent(moveTo.finishEvent.ValueOr(default)); 
                     this.rerunGroup.Value = true;
                 }
             }
         }
-        
-        [BurstCompile]
-        private struct ApplyToTranslations : IJobEntityBatch {
-            [NativeDisableParallelForRestriction]
-            public ComponentDataFromEntity<Translation> allTranslations;
 
-            [ReadOnly]
-            public ComponentTypeHandle<DotsFsmAction> actionHandle;
+        protected override Execution PrepareActionExecution() {
+            // Use the actual deltaTime only when it's the first run of the DotsFsmSystemGroup
+            // Note that we allow reruns on some frames so that some cases like movement will look
+            // smooth. However, if we use the actual deltaTime on every rerun, some entities will
+            // move more than once in every frame. This will look like the agent is moving much
+            // faster that it is supposed to.
+            float deltaTime = this.dotsFsmSystemGroup.RerunCounter == 0 ? UnityEngine.Time.deltaTime : 0;
             
-            public ComponentTypeHandle<MoveTo> moveToHandle;
-
-            public void Execute(ArchetypeChunk batchInChunk, int batchIndex) {
-                NativeArray<DotsFsmAction> actions = batchInChunk.GetNativeArray(this.actionHandle);
-                NativeArray<MoveTo> moveTos = batchInChunk.GetNativeArray(this.moveToHandle);
-                for (int i = 0; i < batchInChunk.Count; ++i) {
-                    DotsFsmAction action = actions[i];
-                    if (!action.running) {
-                        // Apply to only running actions
-                        continue;
-                    }
-                    
-                    MoveTo moveTo = moveTos[i];
-                    
-                    // Apply to translation of target entity
-                    this.allTranslations[moveTo.targetEntity] = new Translation {
-                        Value = moveTo.currentPosition
-                    };
-                }
-            }
+            return new Execution() {
+                allTranslation = GetComponentDataFromEntity<Translation>(),
+                rerunGroup = this.dotsFsmSystemGroup.RerunGroup,
+                deltaTime = deltaTime
+            };
         }
     }
 }

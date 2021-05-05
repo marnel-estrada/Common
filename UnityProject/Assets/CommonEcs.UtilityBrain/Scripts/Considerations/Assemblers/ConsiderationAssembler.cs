a8ea06cf9710701e0111674f6c6f3b4f1d80edaf
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2021-05-05 14:46:53 +0800

    Implemented BonusTuningAssembler

---
+++ UnityProject/Assets/CommonEcs.UtilityBrain/Scripts/Considerations/Assemblers/ConsiderationAssembler.cs
@@ -0,0 +1,12 @@
+using Unity.Collections;
+using Unity.Entities;
+
+namespace CommonEcs.UtilityBrain {
+    public abstract class ConsiderationAssembler {
+        public virtual void Init(ref EntityManager entityManager) {
+        }
+
+        public abstract void Prepare(ref EntityManager entityManager, in Entity agentEntity, in Entity optionEntity, 
+            int optionIndex, ref NativeList<Entity> linkedEntities);
+    }
+}

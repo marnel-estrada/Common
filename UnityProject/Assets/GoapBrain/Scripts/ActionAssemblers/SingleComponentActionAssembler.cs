08ece63e139463c8bd8ff6365fe46fe095a1b306
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2021-05-05 14:46:00 +0800

    SingleComponentConsiderationAssembler

--- UnityProject/Assets/GoapBrain/Scripts/ActionAssemblers/SingleComponentActionAssembler.cs
+++ UnityProject/Assets/GoapBrain/Scripts/ActionAssemblers/SingleComponentActionAssembler.cs
@@ -4,7 +4,7 @@ using Unity.Collections;
 using Unity.Entities;
 
 namespace GoapBrain {
-    public abstract class SingleComponentActionAssembler<T> : AtomActionAssembler where T : unmanaged, IComponentData {
+    public abstract class SingleComponentActionAssembler<T> : AtomActionAssembler where T : unmanaged, IAtomActionComponent {
         private EntityArchetype archetype;
 
         public override void Init(ref EntityManager entityManager, int actionId, int order) {
@@ -16,12 +16,12 @@ namespace GoapBrain {
             Entity actionEntity = entityManager.CreateEntity(this.archetype);
             entityManager.SetComponentData(actionEntity, new AtomAction(this.ActionId, agentEntity, this.Order));
             
-            PrepareAction(ref entityManager, agentEntity, actionEntity);
+            PrepareActionComponent(ref entityManager, agentEntity, actionEntity);
             
             linkedEntities.Add(actionEntity);
         }
 
-        protected virtual void PrepareAction(ref EntityManager entityManager, in Entity agentEntity,
+        protected virtual void PrepareActionComponent(ref EntityManager entityManager, in Entity agentEntity,
             in Entity actionEntity) {
             // May or may not be overridden by deriving class
             // This is needed for cases when the action filter has a custom constructor,

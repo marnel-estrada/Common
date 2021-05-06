c17a9d3be003132336c547a1b0193e458568c23e
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2021-05-06 15:47:57 +0800

    TolerantEquals() for float3

--- UnityProject/Assets/CommonEcs/Scripts/Math/Float3Extensions.cs
+++ UnityProject/Assets/CommonEcs/Scripts/Math/Float3Extensions.cs
@@ -1,5 +1,9 @@
+using Unity.Mathematics;
+
 namespace CommonEcs.Scripts.Math {
-    public class Float3Extensions {
-        
+    public static class Float3Extensions {
+        public static bool TolerantEquals(this float3 a, float3 b) {
+            return a.x.TolerantEquals(b.x) && a.y.TolerantEquals(b.y) && a.z.TolerantEquals(b.z);
+        } 
     }
 }

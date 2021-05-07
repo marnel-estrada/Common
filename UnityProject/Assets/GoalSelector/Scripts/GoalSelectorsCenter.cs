5a93635b201e9eb31f0acbdf82403b166ec218f6
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2021-05-07 15:50:34 +0800

    Implemented parser and runner of consideration assemblers

--- UnityProject/Assets/GoalSelector/Scripts/GoalSelectorsCenter.cs
+++ UnityProject/Assets/GoalSelector/Scripts/GoalSelectorsCenter.cs
@@ -2,8 +2,10 @@ using System.Collections.Generic;
 
 using Common;
 
+using CommonEcs;
 using CommonEcs.UtilityBrain;
 
+using Unity.Collections;
 using Unity.Entities;
 
 using UnityEngine;
@@ -19,19 +21,34 @@ namespace GoalSelector {
         // These are already the parsed data
         private GoalSelector[] selectors;
 
+        private EntityArchetype brainArchetype;
+        private EntityArchetype optionArchetype;
+
         private void Awake() {
             this.selectors = new GoalSelector[this.dataList.Length];
+
+            // Prepare the archetypes
+            EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            this.brainArchetype = entityManager.CreateArchetype(typeof(UtilityBrain), 
+                typeof(UtilityValueWithOption), typeof(LinkedEntityGroup));
             
-            Parse();
+            // Note here that GoalCondition is the custom data that will be need to set the main goal
+            // unto its associated GoapAgent
+            // We added LinkedEntityGroup here for the name
+            this.optionArchetype = entityManager.CreateArchetype(typeof(UtilityOption), 
+                typeof(NameReference), typeof(GoalCondition), typeof(UtilityValue), 
+                typeof(LinkedEntityGroup));
+            
+            Parse(ref entityManager);
         }
 
-        private void Parse() {
+        private void Parse(ref EntityManager entityManager) {
             for (int i = 0; i < this.dataList.Length; ++i) {
-                this.selectors[i] = Parse(this.dataList[i]);
+                this.selectors[i] = Parse(ref entityManager, this.dataList[i]);
             }
         }
 
-        private static GoalSelector Parse(GoalSelectorData data) {
+        private static GoalSelector Parse(ref EntityManager entityManager, GoalSelectorData data) {
             GoalSelector goalSelector = new GoalSelector();
             
             // Parse each goal
@@ -41,6 +58,12 @@ namespace GoalSelector {
                 ParseConsiderations(goalData, goal);
                 
                 goalSelector.Add(goal);
+            }
+            
+            // Initialize assemblers
+            ReadOnlySimpleList<Goal> goals = goalSelector.Goals;
+            for (int i = 0; i < goals.Count; ++i) {
+                InitializeAssemblers(ref entityManager, goals[i]);
             }
 
             return goalSelector;
@@ -70,8 +93,84 @@ namespace GoalSelector {
             }
         }
 
-        public Entity CreateUtilityBrainEntity(ref EntityManager entityManager, int dataIndex) {
-            return Entity.Null;
+        private static void InitializeAssemblers(ref EntityManager entityManager, Goal goal) {
+            ReadOnlySimpleList<ConsiderationAssembler> assemblers = goal.Assemblers;
+            for (int i = 0; i < assemblers.Count; i++) {
+                assemblers[i].Init(ref entityManager);
+            }
+        }
+
+        private readonly SimpleList<Entity> tempLinkedEntities = new SimpleList<Entity>();
+
+        public Entity CreateUtilityBrainEntity(EntityManager entityManager, in Entity agent, int dataIndex) {
+            this.tempLinkedEntities.Clear();
+            
+            Entity brainEntity = CreateBrain(ref entityManager, agent, dataIndex);
+            
+            // Add the brainEntity first as it is the owner of the subsequent entities
+            this.tempLinkedEntities.Add(brainEntity);
+            
+            // Create each option
+            GoalSelector selector = this.selectors[dataIndex];
+            ReadOnlySimpleList<Goal> goals = selector.Goals;
+            for (int i = 0; i < goals.Count; i++) {
+                Entity option = CreateOption(ref entityManager, goals[i], brainEntity, i, agent);
+                this.tempLinkedEntities.Add(option);
+            }
+            
+            // Commit the linked entities
+            DynamicBuffer<LinkedEntityGroup> linkedEntities = entityManager.GetBuffer<LinkedEntityGroup>(brainEntity);
+            for (int i = 0; i < this.tempLinkedEntities.Count; i++) {
+                linkedEntities.Add(this.tempLinkedEntities[i]);
+            }
+            
+            return brainEntity;
+        }
+
+        private Entity CreateBrain(ref EntityManager entityManager, in Entity agent, int dataIndex) {
+            Entity brainEntity = entityManager.CreateEntity(this.brainArchetype);
+            
+            // Note here that we pass true so that UtilityBrain will execute upon creation
+            entityManager.SetComponentData(brainEntity, new UtilityBrain(agent, true));
+            
+            // Prepare enough slots of UtilityValueWithOption
+            DynamicBuffer<UtilityValueWithOption> brainValueList = entityManager.GetBuffer<UtilityValueWithOption>(brainEntity);
+            GoalSelector selector = this.selectors[dataIndex];
+            for (int i = 0; i < selector.GoalCount; ++i) {
+                brainValueList.Add(default);
+            }
+
+            return brainEntity;
+        }
+
+        private Entity CreateOption(ref EntityManager entityManager, Goal goal, in Entity parentBrain, int brainIndex, in Entity agent) {
+            Entity option = entityManager.CreateEntity(this.optionArchetype);
+            entityManager.SetComponentData(option, new UtilityOption(goal.Id, parentBrain, brainIndex));
+            entityManager.SetComponentData(option, new GoalCondition(goal.ConditionName, goal.ConditionValue));
+            Name.SetName(ref entityManager, option, goal.Id);
+
+            ReadOnlySimpleList<ConsiderationAssembler> assemblers = goal.Assemblers;
+            
+            // Prepare enough UtilityValue slots for considerations 
+            DynamicBuffer<UtilityValue> optionValueList = entityManager.GetBuffer<UtilityValue>(option);
+            for (int i = 0; i < assemblers.Count; ++i) {
+                optionValueList.Add(default);    
+            }
+            
+            // Prepare consideration entity for each assembler
+            // This is needed as ConsiderationAssembler.Prepare() needs this
+            NativeList<Entity> optionLinkedEntities = new NativeList<Entity>(Allocator.Temp);
+            for (int i = 0; i < assemblers.Count; ++i) {
+                ConsiderationAssembler assembler = assemblers[i];
+                assembler.Prepare(ref entityManager, agent, option, i, ref optionLinkedEntities);
+            }
+            
+            // Add entries from optionLinkedEntities to tempLinkedEntities
+            for (int i = 0; i < optionLinkedEntities.Length; i++) {
+                this.tempLinkedEntities.Add(optionLinkedEntities[i]);
+            }
+
+            return option;
         }
     }
 }

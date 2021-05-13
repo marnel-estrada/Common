5d49d4b57a5ab9377f61d2b3a04e2b267de4cb97
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2021-05-13 21:13:16 +0800

    EntityQueryDescBuilder

---
+++ UnityProject/Assets/CommonEcs/Scripts/EntityQueryDescBuilder.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+
+using Unity.Entities;
+
+namespace CommonEcs {
+    /// <summary>
+    /// A utility class for building EntityQueryDesc using chained calls.
+    /// </summary>
+    public class EntityQueryDescBuilder {
+        private readonly List<ComponentType> all = new List<ComponentType>();
+        private readonly List<ComponentType> any = new List<ComponentType>();
+        private readonly List<ComponentType> none = new List<ComponentType>();
+        
+        private EntityQueryDescBuilder() {
+        }
+
+        public static EntityQueryDescBuilder New() {
+            return new EntityQueryDescBuilder();
+        }
+
+        public EntityQueryDescBuilder With<T>() where T : IComponentData {
+            this.all.Add(ComponentType.ReadWrite<T>());
+            return this;
+        }
+
+        public EntityQueryDescBuilder WithReadOnly<T>() where T : unmanaged, IComponentData {
+            this.all.Add(ComponentType.ReadOnly<T>());
+            return this;
+        }
+
+        public EntityQueryDescBuilder WithAny<T>() where T : IComponentData {
+            this.any.Add(ComponentType.ReadWrite<T>());
+            return this;
+        }
+
+        public EntityQueryDescBuilder WithAnyReadOnly<T>() where T : unmanaged, IComponentData {
+            this.any.Add(ComponentType.ReadOnly<T>());
+            return this;
+        }
+
+        public EntityQueryDescBuilder WithNone<T>() where T : IComponentData {
+            this.none.Add(ComponentType.Exclude<T>());
+            return this;
+        }
+
+        public EntityQueryDesc Complete() {
+            return new EntityQueryDesc() {
+                Any = this.any.Count > 0 ? this.any.ToArray() : Array.Empty<ComponentType>(),
+                None = this.none.Count > 0 ? this.none.ToArray() : Array.Empty<ComponentType>(),
+                All = this.all.Count > 0 ? this.all.ToArray() : Array.Empty<ComponentType>()
+            };
+        }
+    }
+}

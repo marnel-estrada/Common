518dfd5e802b37a47ec19bade146daabe5f69abc
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2021-06-08 13:50:08 +0800

    Added GenerateAuthoringComponent so it could be added to prefabs

--- UnityProject/Assets/CommonEcs.Rendering/Scripts/Components/UseYAsSortOrder.cs
+++ UnityProject/Assets/CommonEcs.Rendering/Scripts/Components/UseYAsSortOrder.cs
@@ -1,20 +1,21 @@
-﻿namespace CommonEcs {
-    using System;
+﻿using System;
 
-    using Unity.Entities;
-    
+using Unity.Entities;
+
+namespace CommonEcs {
     /// <summary>
     /// A component that identifies whether or not to use Y as the sort order
     /// </summary>
     [Serializable]
-    public struct UseYAsSortOrder : IComponentData {
+    [GenerateAuthoringComponent]
+    public readonly struct UseYAsSortOrder : IComponentData {
         // We may want to offset the y position such that we don't have to rely on a parent
         // transform to get the correct y value.
         // This is for cases like in Academia where characters have body sprite and head sprite.
         // If we didn't have an offset, the head sprite will use its own y position and will be sorted 
         // independently from its body.
         // We need a mechanism such that the head sprite and body sprite have the same y value.
-        public float offset;
+        public readonly float offset;
 
         public UseYAsSortOrder(float offset) {
             this.offset = offset;

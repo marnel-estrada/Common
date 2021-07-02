c90174bfdcab03582acfa59d453dd95df1101cac
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2021-07-02 14:19:00 +0800

    Added CLEANUP state

--- UnityProject/Assets/CommonEcs.Goap/Scripts/Components/AgentState.cs
+++ UnityProject/Assets/CommonEcs.Goap/Scripts/Components/AgentState.cs
@@ -2,6 +2,12 @@ namespace CommonEcs.Goap {
     public enum AgentState : byte {
         IDLE,
         PLANNING,
-        EXECUTING // Executing actions
+        EXECUTING, // Executing actions
+        
+        // This is the state after agent has executed its current action, failed or success.
+        // It's also a mark where atom actions need to revert their state if they need to.
+        // Replanning will set the agent's state to this so that atom action can clean up before
+        // proceeding to the next plan.
+        CLEANUP
     }
 }

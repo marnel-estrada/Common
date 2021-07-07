b29898d5287be56ce79bcb4d719ce898dc9f9992
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2021-07-07 13:45:07 +0800

    Added generic rendering for list of strings

--- UnityProject/Assets/CommonCore/Editor/EditorRenderUtils.cs
+++ UnityProject/Assets/CommonCore/Editor/EditorRenderUtils.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+
+using UnityEngine;
 using UnityEditor;
 
 namespace Common {
@@ -22,6 +24,65 @@ namespace Common {
 
             index = EditorGUILayout.Popup(index, valueSet.DisplayList, GUILayout.Width(width));
             return valueSet.GetValue(index);
+        }
+
+        private static string NEW_ENTRY = "";
+        private static readonly SimpleList<string> REMOVAL_LIST = new SimpleList<string>();
+
+        /// <summary>
+        /// Generic renderer for a string list
+        /// Returns whether or not there changes on the list
+        /// </summary>
+        /// <param name="stringList"></param>
+        public static bool Render(string title, List<string> stringList) {
+            bool changed = false;
+            
+            // New entry
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("New: ", GUILayout.Width(40));
+            NEW_ENTRY = GUILayout.TextField(NEW_ENTRY, GUILayout.Width(200));
+            if (GUILayout.Button("Add", GUILayout.Width(50))) {
+                if (!string.IsNullOrEmpty(NEW_ENTRY)) {
+                    stringList.Add(NEW_ENTRY);
+                    changed = true;
+                }
+            }
+            GUILayout.EndHorizontal();
+            
+            GUILayout.Space(5);
+            
+            // Existing entries
+            for (int i = 0; i < stringList.Count; ++i) {
+                GUILayout.BeginHorizontal();
+                
+                // Close button
+                GUI.backgroundColor = Color.red;
+                if (GUILayout.Button("X", GUILayout.Width(20))) {
+                    if (EditorUtility.DisplayDialogComplex(title, $"Are you sure you want to remove {stringList[i]}?", "Yes", "No", "Cancel") == 0) {
+                        REMOVAL_LIST.Add(stringList[i]);
+                    }
+                }
+                GUI.backgroundColor = Color.white;
+                
+                string current = stringList[i];
+                string field = GUILayout.TextField(current, GUILayout.Width(200));
+                if (!current.EqualsFast(field)) {
+                    // There was a rename
+                    stringList[i] = field;
+                    changed = true;
+                }
+                
+                GUILayout.EndHorizontal();
+            }
+            
+            // Apply removal list
+            changed = changed || REMOVAL_LIST.Count > 0; 
+            for (int i = 0; i < REMOVAL_LIST.Count; ++i) {
+                stringList.Remove(REMOVAL_LIST[i]);
+            }
+            REMOVAL_LIST.Clear();
+
+            return changed;
         }
     }
 }

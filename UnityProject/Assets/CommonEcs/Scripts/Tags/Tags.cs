5d83404098cf2fba82a84f5ec16fa049bb267887
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2021-07-08 14:58:32 +0800

    Added Contains() that accepts another Tags (multiple tags)

--- UnityProject/Assets/CommonEcs/Scripts/Tags/Tags.cs
+++ UnityProject/Assets/CommonEcs/Scripts/Tags/Tags.cs
@@ -27,5 +27,14 @@ namespace CommonEcs {
             int index = tagSet.GetIndex(tagHashCode);
             return this.values[index];
         }
+
+        public bool Contains(in TagSet tagSet, in Tags tagsToCheck) {
+            DotsAssert.IsTrue(tagSet.id == this.tagSetId); // Ensure that we don't use another TagSet
+            DotsAssert.IsTrue(tagSet.id == tagsToCheck.tagSetId); // Ensure that we don't use another TagSet
+            
+            // We can simply check if the tags in tagsToCheck are contained
+            // by using & on the bit array and check if it's equal (meaning all bits were retained)
+            return (this.values.InternalValue & tagsToCheck.values.InternalValue) == tagsToCheck.values.InternalValue;
+        }
     }
 }

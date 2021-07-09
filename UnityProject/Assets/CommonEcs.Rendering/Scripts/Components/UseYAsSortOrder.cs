a69ed77665210c13c416312db04dd87f52e8f8d7
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2021-07-09 15:25:25 +0800

    Removed readonly so it can be set in prefab

--- UnityProject/Assets/CommonEcs.Rendering/Scripts/Components/UseYAsSortOrder.cs
+++ UnityProject/Assets/CommonEcs.Rendering/Scripts/Components/UseYAsSortOrder.cs
@@ -8,14 +8,14 @@ namespace CommonEcs {
     /// </summary>
     [Serializable]
     [GenerateAuthoringComponent]
-    public readonly struct UseYAsSortOrder : IComponentData {
+    public struct UseYAsSortOrder : IComponentData {
         // We may want to offset the y position such that we don't have to rely on a parent
         // transform to get the correct y value.
         // This is for cases like in Academia where characters have body sprite and head sprite.
         // If we didn't have an offset, the head sprite will use its own y position and will be sorted 
         // independently from its body.
         // We need a mechanism such that the head sprite and body sprite have the same y value.
-        public readonly float offset;
+        public float offset;
 
         public UseYAsSortOrder(float offset) {
             this.offset = offset;

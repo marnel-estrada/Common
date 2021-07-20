44bd0643bb5e68f6a3f961fb434ee26ad1bc2fb8
neilmarkcorre <nmgcorre@gmail.com>
2021-07-20 16:45:21 +0800

    Revert "Removed tagset id checking to allow for existence checking between two tagsets (ex. checking object static data tags' existence in a zone static data tagset)"
    This reverts commit 6d5149f46db2a4e143751a92795841453a30b00f.

--- UnityProject/Assets/CommonEcs/Scripts/Tags/Tags.cs
+++ UnityProject/Assets/CommonEcs/Scripts/Tags/Tags.cs
@@ -32,7 +32,10 @@ namespace CommonEcs {
             return this.values[index];
         }
 
-        public readonly bool Contains(in Tags tagsToCheck) {
+        public readonly bool Contains(in TagSet tagSet, in Tags tagsToCheck) {
+            DotsAssert.IsTrue(tagSet.id == this.tagSetId); // Ensure that we don't use another TagSet
+            DotsAssert.IsTrue(tagSet.id == tagsToCheck.tagSetId); // Ensure that we don't use another TagSet
+
             // We can simply check if the tags in tagsToCheck are contained
             // by using & on the bit array and check if it's equal (meaning all bits were retained)
             return (this.values.InternalValue & tagsToCheck.values.InternalValue) == tagsToCheck.values.InternalValue;

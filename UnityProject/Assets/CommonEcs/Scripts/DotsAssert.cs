85e78165480bce07f1a1e29989009dd5f469b674
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2021-07-23 20:51:23 +0800

    Added compiler directive

--- UnityProject/Assets/CommonEcs/Scripts/DotsAssert.cs
+++ UnityProject/Assets/CommonEcs/Scripts/DotsAssert.cs
@@ -6,13 +6,17 @@ namespace CommonEcs {
     public struct DotsAssert {
         public static void IsTrue(bool condition, FixedString128 message = new FixedString128()) {
             if (!condition) {
+#if UNITY_EDITOR
                 throw new Exception(message.ToString());
+#endif
             }
         }
         
         public static void IsFalse(bool condition, FixedString128 message = new FixedString128()) {
             if (condition) {
+#if UNITY_EDITOR
                 throw new Exception(message.ToString());
+#endif                
             }
         }
     }

1306adf81a7fc599e541f8f18386e82d57114eb5
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2021-08-06 17:03:15 +0800

    Marked DebugEntity as Serializable

--- UnityProject/Assets/CommonEcs/Scripts/Components/DebugEntity.cs
+++ UnityProject/Assets/CommonEcs/Scripts/Components/DebugEntity.cs
@@ -1,11 +1,17 @@
-﻿using Unity.Entities;
+﻿using System;
+
+using Unity.Entities;
+
+using UnityEngine;
 
 namespace CommonEcs {
     /// <summary>
     /// A common component that can be used as a filter to debug certain entities
     /// </summary>
+    [Serializable]
     [GenerateAuthoringComponent]
     public struct DebugEntity : IComponentData {
+        [SerializeField]
         public bool isDebug;
 
         public DebugEntity(bool isDebug) {

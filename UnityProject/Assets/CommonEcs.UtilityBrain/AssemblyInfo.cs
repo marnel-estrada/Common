7a22eb60c2dca59a0c25f5408b1abb98871b01c9
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2021-08-06 17:02:45 +0800

    Added RankTuning

--- UnityProject/Assets/CommonEcs.UtilityBrain/AssemblyInfo.cs
+++ UnityProject/Assets/CommonEcs.UtilityBrain/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using CommonEcs.UtilityBrain;
 using Unity.Entities;
 
 [assembly:RegisterGenericJobType(typeof(ConsiderationBaseSystem<BonusTuning, BonusTuningSystem.Processor>.Job))]
+[assembly:RegisterGenericJobType(typeof(ConsiderationBaseSystem<RankTuning, RankTuningSystem.Processor>.Job))]

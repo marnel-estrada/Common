70fd131dec6a84c1fdeaab39ef811601ce97193e
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2021-08-25 16:39:39 +0800

    Added overloads with no message; Added NotNullEntity();

--- UnityProject/Assets/CommonEcs/Scripts/DotsAssert.cs
+++ UnityProject/Assets/CommonEcs/Scripts/DotsAssert.cs
@@ -1,10 +1,25 @@
 using System;
 
 using Unity.Collections;
+using Unity.Entities;
 
 namespace CommonEcs {
     public struct DotsAssert {
-        public static void IsTrue(bool condition, FixedString128 message = new FixedString128()) {
+        /// <summary>
+        /// We provided an overload instead of default value so that the method will not create
+        /// dummy FixedString128 for empty messages
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <exception cref="Exception"></exception>
+        public static void IsTrue(bool condition) {
+            if (!condition) {
+#if UNITY_EDITOR
+                throw new Exception();
+#endif
+            }
+        }
+        
+        public static void IsTrue(bool condition, FixedString128 message) {
             if (!condition) {
 #if UNITY_EDITOR
                 throw new Exception(message.ToString());
@@ -12,12 +27,30 @@ namespace CommonEcs {
             }
         }
         
-        public static void IsFalse(bool condition, FixedString128 message = new FixedString128()) {
+        /// <summary>
+        /// We provided an overload instead of default value so that the method will not create
+        /// dummy FixedString128 for empty messages
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <exception cref="Exception"></exception>
+        public static void IsFalse(bool condition) {
+            if (condition) {
+#if UNITY_EDITOR
+                throw new Exception();
+#endif                
+            }
+        }
+        
+        public static void IsFalse(bool condition, FixedString128 message) {
             if (condition) {
 #if UNITY_EDITOR
                 throw new Exception(message.ToString());
 #endif                
             }
+        }
+
+        public static void NotNullEntity(in Entity entity) {
+            IsTrue(entity != Entity.Null);
         }
     }
 }

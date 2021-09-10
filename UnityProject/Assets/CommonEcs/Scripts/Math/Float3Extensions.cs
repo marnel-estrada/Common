eff80fb4fea221b8d62ab061109c7bb21b4ef47e
neilmarkcorre <nmgcorre@gmail.com>
2021-09-10 14:30:20 +0800

    Added TolerantGreaterThanOrEquals and TolerantLessThanOrEquals to the float3 extensions

--- UnityProject/Assets/CommonEcs/Scripts/Math/Float3Extensions.cs
+++ UnityProject/Assets/CommonEcs/Scripts/Math/Float3Extensions.cs
@@ -4,6 +4,14 @@ namespace CommonEcs.Scripts.Math {
     public static class Float3Extensions {
         public static bool TolerantEquals(this float3 a, float3 b) {
             return a.x.TolerantEquals(b.x) && a.y.TolerantEquals(b.y) && a.z.TolerantEquals(b.z);
-        } 
+        }
+
+        public static bool TolerantLessThanOrEquals(this float3 a, float3 b) {
+            return a.x.TolerantLesserThanOrEquals(b.x) && a.y.TolerantLesserThanOrEquals(b.y) && a.z.TolerantLesserThanOrEquals(b.z);
+        }
+
+        public static bool TolerantGreaterThanOrEquals(this float3 a, float3 b) {
+            return a.x.TolerantGreaterThanOrEquals(b.x) && a.y.TolerantGreaterThanOrEquals(b.y) && a.z.TolerantGreaterThanOrEquals(b.z);
+        }
     }
 }

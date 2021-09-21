1a008dd967ca963e5f7bc29b441a44ba9fbcb5a5
neilmarkcorre <nmgcorre@gmail.com>
2021-09-21 18:03:13 +0800

    Removed redundant Repaint call as Init already calls Repaint

--- UnityProject/Assets/GoalSelector/Editor/GoalSelectorEditor.cs
+++ UnityProject/Assets/GoalSelector/Editor/GoalSelectorEditor.cs
@@ -5,15 +5,13 @@ using UnityEditor;
 using UnityEngine;
 
 namespace GoalSelector.Editor {
-    [CustomEditor(typeof(GoalSelectorData))]
     public class GoalSelectorEditor : DataPoolEditor<GoalData> {
         public override void OnInspectorGUI() {
             base.OnInspectorGUI();
-
+            
             if (GUILayout.Button("Open Editor")) {
                 GoalSelectorEditorWindow window = EditorWindow.GetWindow<GoalSelectorEditorWindow>("GoalSelector Editor");
                 window.Init(this.DataPool, new GoalDataRenderer(window));
-                window.Repaint();
             }
         }
     }

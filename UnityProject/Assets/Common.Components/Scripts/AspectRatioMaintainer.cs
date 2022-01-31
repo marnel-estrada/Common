a024275ffa4b8e8d89e5faa4aad300bc84fb40ec
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2022-01-31 20:10:57 +0800

    AspectRatioMaintainer

---
+++ UnityProject/Assets/Common.Components/Scripts/AspectRatioMaintainer.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace Common {
+    /// <summary>
+    /// Maintains aspect ratio in different resolutions.
+    /// </summary>
+    public class AspectRatioMaintainer : MonoBehaviour {
+        [SerializeField]
+        private int targetResolutionWidth = 9;
+        
+        [SerializeField]
+        private int targetResolutionHeight = 16;
+
+        [SerializeField]
+        private new Camera camera; // This is the camera to alter
+
+        private void Start() {
+            Assertion.NotNull(this.camera);
+            
+            float targetAspect = this.targetResolutionWidth / (float)this.targetResolutionHeight;
+            float currentAspect = Screen.width / (float)Screen.height;
+            float rectHeight = currentAspect / targetAspect;
+
+            Rect cameraRect = this.camera.rect;
+            cameraRect.height = rectHeight;
+            cameraRect.y = (1.0f - rectHeight) / 2.0f;
+            this.camera.rect = cameraRect;
+        }
+    }
+}

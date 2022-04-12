aa2fefe08a11ae3ba1953fd549eeb563da98023c
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2022-04-12 11:06:08 +0800

    Removed [NotNull]

--- UnityProject/Assets/GoalSelector/Editor/ConsiderationsView.cs
+++ UnityProject/Assets/GoalSelector/Editor/ConsiderationsView.cs
@@ -3,13 +3,11 @@ using Common.Signal;
 
 using CommonEcs.UtilityBrain;
 
-using JetBrains.Annotations;
-
 using UnityEditor;
 
 namespace GoalSelector.Editor {
     public class ConsiderationsView : ClassDataView<ConsiderationAssembler, ConsiderationsAssemblerBrowserWindow> {
-        public ConsiderationsView([NotNull] EditorWindow parent, [NotNull] Signal repaintSignal) : base(parent, repaintSignal) {
+        public ConsiderationsView(EditorWindow parent, Signal repaintSignal) : base(parent, repaintSignal) {
         }
     }
 }

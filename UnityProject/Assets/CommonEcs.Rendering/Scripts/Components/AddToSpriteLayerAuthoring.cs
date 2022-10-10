8eb1032fd2e96bc46dcaa171176d88aa9531593c
Marnel-PC\Marnel <marnel.estrada@gmail.com>
2022-10-10 19:54:46 +0800

    Removed GenerateAuthoringComponent

--- UnityProject/Assets/CommonEcs.Rendering/Scripts/Components/AddToSpriteLayerAuthoring.cs
+++ UnityProject/Assets/CommonEcs.Rendering/Scripts/Components/AddToSpriteLayerAuthoring.cs
@@ -1,15 +1,20 @@
 ﻿using Unity.Entities;
 
+using UnityEngine;
+
 namespace CommonEcs {
     /// <summary>
     /// A component that identifies a certain sprite to add to sprite layer
     /// </summary>
-    [GenerateAuthoringComponent]
     public struct AddToSpriteLayer : IComponentData {
         public Entity layerEntity;
 
         public AddToSpriteLayer(Entity layerEntity) {
             this.layerEntity = layerEntity;
         }
+    }
+
+    public class AddToSpriteLayerAuthoring : MonoBehaviour {
+        public GameObject layerEntity;
     }
 }
--- UnityProject/Assets/CommonEcs.UtilityBrain/Scripts/Components/UtilityBrainReferenceAuthoring.cs
+++ UnityProject/Assets/CommonEcs.UtilityBrain/Scripts/Components/UtilityBrainReferenceAuthoring.cs
@@ -1,15 +1,20 @@
 using Unity.Entities;
 
+using UnityEngine;
+
 namespace CommonEcs.UtilityBrain {
     /// <summary>
     /// A component that can be added to entities that requires reference to a utility brain entity.
     /// </summary>
-    [GenerateAuthoringComponent]
     public struct UtilityBrainReference : IComponentData {
         public NonNullEntity utilityBrainEntity;
 
         public UtilityBrainReference(Entity utilityBrainEntity) {
             this.utilityBrainEntity = utilityBrainEntity;
         }
+    }
+
+    public class UtilityBrainReferenceAuthoring : MonoBehaviour {
+        public NonNullEntity utilityBrainEntity;
     }
 }

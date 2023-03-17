dd5f367bacac97489dd53fcbdc090521650ea1cf
Marnielle Lloyd Estrada <marnel.estrada@gmail.com>
2023-03-17 08:31:35 +0100

    Implemented EntityIndexAide as a utility for calculating the entity index in query

---
+++ UnityProject/Assets/CommonEcs/Scripts/EntityIndexAide.cs
@@ -0,0 +1,24 @@
+using Unity.Collections;
+
+namespace CommonEcs {
+    /// <summary>
+    /// This is utility for getting the entity index when doing chunk iteration
+    /// We did this because it's so cumbersome to write for the moment
+    /// This is based from https://docs.unity3d.com/Packages/com.unity.entities@1.0/manual/upgrade-guide.html#jobs-that-need-firstentityindex
+    /// </summary>
+    public struct EntityIndexAide {
+        private readonly int baseEntityIndex;
+        private int validEntitiesInChunk;
+
+        public EntityIndexAide(NativeArray<int> chunkBaseEntityIndices, int unfilteredChunkIndex) : this() {
+            this.baseEntityIndex = chunkBaseEntityIndices[unfilteredChunkIndex];
+        }
+
+        public int NextEntityIndexInQuery() {
+            int entityIndexInQuery = this.baseEntityIndex + this.validEntitiesInChunk;
+            ++this.validEntitiesInChunk;
+
+            return entityIndexInQuery;
+        }
+    }
+}

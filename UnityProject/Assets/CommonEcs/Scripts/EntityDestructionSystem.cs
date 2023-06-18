5b461270567589dfb2da5d21b000b1d34ac19fec
Marnielle Lloyd Estrada <marnel.estrada@gmail.com>
2023-06-18 18:30:30 +0200

    Use an EntityCommandBuffer to destroy entities instead of EntityManager

--- UnityProject/Assets/CommonEcs/Scripts/EntityDestructionSystem.cs
+++ UnityProject/Assets/CommonEcs/Scripts/EntityDestructionSystem.cs
@@ -3,13 +3,18 @@
 namespace CommonEcs {
     public partial class EntityDestructionSystem : SystemBase {
         private EntityQuery query;
+        
+        private EntityCommandBufferSystem commandBufferSystem;
 
         protected override void OnCreate() {
             this.query = GetEntityQuery(typeof(ForDestruction));
+            
+            this.commandBufferSystem = this.GetOrCreateSystemManaged<EndSimulationEntityCommandBufferSystem>();
         }
 
         protected override void OnUpdate() {
-            this.EntityManager.DestroyEntity(this.query);
+            this.commandBufferSystem.CreateCommandBuffer().DestroyEntity(this.query);
+            this.commandBufferSystem.AddJobHandleForProducer(this.Dependency);
         }
     }
 }

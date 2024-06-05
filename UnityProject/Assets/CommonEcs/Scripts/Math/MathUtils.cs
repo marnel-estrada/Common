9a2b94aea50851fcb95e3a5e324042909ac6b507
marnel.estrada <marnel.estrada@gmail.com>
2024-06-05 08:54:01 +0200

    Added MathUtils

---
+++ UnityProject/Assets/CommonEcs/Scripts/Math/MathUtils.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Mathematics;
+
+namespace Common {
+    public static class MathUtils {
+        public static float2 InsideUnitCircle(ref Random random) {
+            float angle = random.NextFloat(0, math.PI * 2);
+            float radius = random.NextFloat(0.0f, 1.0f);
+
+            float x = radius * math.cos(angle);
+            float y = radius * math.sin(angle);
+
+            return new float2(x, y);
+        }
+    }
+}

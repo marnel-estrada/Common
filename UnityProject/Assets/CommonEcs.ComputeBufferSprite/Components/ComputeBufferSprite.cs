224480ab213daa252d308d56e94dec124b643033
marnel.estrada <marnel.estrada@gmail.com>
2024-06-18 08:38:26 +0200

    Upgrade to Entities 1.2.3

---
+++ UnityProject/Assets/CommonEcs.ComputeBufferSprite/Components/ComputeBufferSprite.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace CommonEcs {
+    public struct ComputeBufferSprite : IComponentData {
+        public Color color;
+
+        // Higher order means rendered later. Value can be negative.
+        public int layerOrder;
+    }
+}

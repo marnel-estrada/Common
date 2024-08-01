a5077c022a49d7eb48381a2386e892d5b4c0a75f
marnel.estrada <marnel.estrada@gmail.com>
2024-08-01 09:12:24 +0200

    Reverted TolerantLessThanOrEquals() and TolerantGreaterThanOrEquals() since they are used in CityHallSimulator

--- UnityProject/Assets/CommonEcs/Scripts/Math/FloatXExtensions.cs
+++ UnityProject/Assets/CommonEcs/Scripts/Math/FloatXExtensions.cs
@@ -20,6 +20,18 @@ namespace CommonEcs.Math {
             return a.x.TolerantEquals(b.x) && a.y.TolerantEquals(b.y) && a.z.TolerantEquals(b.z);
         }
 
+        public static bool TolerantLessThanOrEquals(this float3 a, float3 b) {
+            return a.x.TolerantLesserThanOrEquals(b.x) && 
+                   a.y.TolerantLesserThanOrEquals(b.y) &&
+                   a.z.TolerantLesserThanOrEquals(b.z);
+        }
+
+        public static bool TolerantGreaterThanOrEquals(this float3 a, float3 b) {
+            return a.x.TolerantGreaterThanOrEquals(b.x) && 
+                   a.y.TolerantGreaterThanOrEquals(b.y) &&
+                   a.z.TolerantGreaterThanOrEquals(b.z);
+        }
+
         public static bool IsZero(this float3 v) {
             return Comparison.IsZero(v.x) && Comparison.IsZero(v.y) && Comparison.IsZero(v.z);
         }

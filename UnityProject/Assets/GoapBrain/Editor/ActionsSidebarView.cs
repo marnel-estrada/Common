05a9b56a074ca74e16880c48d3fde1f90ec0e559
marnel.estrada <marnel.estrada@gmail.com>
2024-10-05 15:43:31 +0200

    Increased width

--- UnityProject/Assets/GoapBrain/Editor/ActionsSidebarView.cs
+++ UnityProject/Assets/GoapBrain/Editor/ActionsSidebarView.cs
@@ -32,7 +32,7 @@ namespace GoapBrain {
         /// </summary>
         /// <param name="domainData"></param>
         public void Render(GoapDomainData domainData) {
-            EditorGUILayout.BeginVertical(GUILayout.Width(250));
+            EditorGUILayout.BeginVertical(GUILayout.Width(350));
 
             this.scrollPos = EditorGUILayout.BeginScrollView(this.scrollPos);
 

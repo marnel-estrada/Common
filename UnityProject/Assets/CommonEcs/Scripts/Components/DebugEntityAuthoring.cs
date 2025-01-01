27ffe5358dd9f3869661bb6a221228e10bf8436b
marnel.estrada <marnel.estrada@gmail.com>
2025-01-01 19:58:13 +0100

    Made readonly

--- UnityProject/Assets/CommonEcs/Scripts/Components/DebugEntityAuthoring.cs
+++ UnityProject/Assets/CommonEcs/Scripts/Components/DebugEntityAuthoring.cs
@@ -1,13 +1,12 @@
 ﻿using Unity.Entities;
-
 using UnityEngine;
 
 namespace CommonEcs {
     /// <summary>
     /// A common component that can be used as a filter to debug certain entities
     /// </summary>
-    public struct DebugEntity : IComponentData {
-        public bool enabled;
+    public readonly struct DebugEntity : IComponentData {
+        public readonly bool enabled;
 
         public DebugEntity(bool enabled) {
             this.enabled = enabled;
@@ -15,12 +14,12 @@ namespace CommonEcs {
     }
 
     public class DebugEntityAuthoring : MonoBehaviour {
-        public bool enabled;
+        public bool debugEnabled;
         
         internal class Baker : Baker<DebugEntityAuthoring> {
             public override void Bake(DebugEntityAuthoring authoring) {
                 Entity primaryEntity = GetEntity(TransformUsageFlags.None);
-                AddComponent(primaryEntity, new DebugEntity(authoring.enabled));
+                AddComponent(primaryEntity, new DebugEntity(authoring.debugEnabled));
             }
         }
     }

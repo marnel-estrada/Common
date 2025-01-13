68e25fb4be3099b84f601ce7becdf5b28021ff91
marnel.estrada <marnel.estrada@gmail.com>
2025-01-13 09:30:44 +0100

    SourcePrefab component

---
+++ UnityProject/Assets/CommonEcs/Scripts/Components/SourcePrefab.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Entities;
+
+namespace Components {
+    /// <summary>
+    /// A component that stores the source prefab of an entity. It can be used in serialization
+    /// to spawn such prefab. The idHashCode here is the hash code of the text ID of the prefab.
+    /// </summary>
+    public readonly struct SourcePrefab : IComponentData {
+        public readonly int idHashCode;
+
+        public SourcePrefab(int idHashCode) {
+            this.idHashCode = idHashCode;
+        }
+    }
+}

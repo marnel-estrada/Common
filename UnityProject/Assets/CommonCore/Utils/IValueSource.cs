72476fa382d613b04c54edf9e0010c3496104977
marnel.estrada <marnel.estrada@gmail.com>
2025-01-31 11:52:56 +0100

    Common interface IValueSource

---
+++ UnityProject/Assets/CommonCore/Utils/IValueSource.cs
@@ -0,0 +1,9 @@
+﻿namespace Common {
+    /// <summary>
+    /// A common interface to get a certain value from.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IValueSource<out T> {
+        T Value { get; }
+    }
+}

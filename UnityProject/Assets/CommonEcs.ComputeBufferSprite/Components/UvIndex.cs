eacaff78743bb853c582c1edb1d89075c8f2ebc4
marnel.estrada <marnel.estrada@gmail.com>
2025-02-07 09:45:22 +0100

    Added DynamicBufferExtensions

--- UnityProject/Assets/CommonEcs.ComputeBufferSprite/Components/UvIndex.cs
+++ UnityProject/Assets/CommonEcs.ComputeBufferSprite/Components/UvIndex.cs
@@ -5,7 +5,7 @@ namespace CommonEcs {
     /// An index into a UV array. We implemented this as an IBufferElementData as we may want multiple
     /// UVs like in case of heads in Academia: School Simulator.
     /// </summary>
-    [InternalBufferCapacity(2)]
+    [InternalBufferCapacity(1)]
     public struct UvIndex : IBufferElementData {
         public readonly int value;
 
---
+++ UnityProject/Assets/CommonEcs/Scripts/DynamicBufferExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Entities;
+
+namespace CommonEcs {
+    public static class DynamicBufferExtensions {
+        /// <summary>
+        /// Sets the value at index 0 if such value was already added. If not, it adds the value.
+        /// </summary>
+        /// <param name="self"></param>
+        /// <param name="value"></param>
+        /// <typeparam name="T"></typeparam>
+        public static void SetFirstValueOrAdd<T>(this DynamicBuffer<T> self, T value)
+            where T : unmanaged, IBufferElementData {
+            if (self.Length == 0) {
+                self.Add(value);
+                return;
+            }
+
+            // Already has content
+            self[0] = value;
+        }
+    }
+}

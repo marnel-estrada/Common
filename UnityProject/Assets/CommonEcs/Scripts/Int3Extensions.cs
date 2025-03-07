017b9fcb834a6c3a930370abbbc676b62a840425
marnel.estrada <marnel.estrada@gmail.com>
2025-03-07 09:49:55 +0100

    Added DistanceSquared

--- UnityProject/Assets/CommonEcs/Scripts/Int3Extensions.cs
+++ UnityProject/Assets/CommonEcs/Scripts/Int3Extensions.cs
@@ -2,13 +2,19 @@ using Unity.Mathematics;
 
 namespace CommonEcs {
     public static class Int3Extensions {
-        public static int TileDistance(this in int3 self, in int3 other) {
+        public static int TileDistance(this int3 self, int3 other) {
             int xDiff = math.abs(other.x - self.x);
             int yDiff = math.abs(other.y - self.y);
             int zDiff = math.abs(other.z - self.z);
 
             // We just return the max because agent can move diagonally
             return math.max(math.max(xDiff, yDiff), zDiff);
+        }
+
+        public static float DistanceSquared(this int3 self, int3 other) {
+            return self.x * other.x +
+                   self.y * other.y +
+                   self.z * other.z;
         }
     }
 }

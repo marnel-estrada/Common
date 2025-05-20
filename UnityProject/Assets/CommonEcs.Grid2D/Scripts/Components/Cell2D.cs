a323d4fb74b7edef7d1410ddfe5cee7d235974e2
marnel.estrada <marnel.estrada@gmail.com>
2025-05-20 10:18:58 +0200

    Use int3 for the grid coordinate

--- UnityProject/Assets/CommonEcs.Grid2D/Scripts/Components/Cell2D.cs
+++ UnityProject/Assets/CommonEcs.Grid2D/Scripts/Components/Cell2D.cs
@@ -6,14 +6,15 @@ namespace CommonEcs {
     /// A component that represents a cell in a 2D grid
     /// </summary>
     public readonly struct Cell2D : IComponentData {
+        // Note that this is 3D since the z value refers to the floor level
+        public readonly int3 gridCoordinate; // This is the coordinate where (0, 0) is at the bottom left of the grid
         public readonly float2 center;
         public readonly float2 size;
         public readonly int2 worldCoordinate; // This is the coordinate where (0, 0) is at the center
-        public readonly int2 gridCoordinate; // This is the coordinate where (0, 0) is at the bottom left of the grid
         public readonly Entity grid; // Reference to the grid that owns this cell
         public readonly int index; // Index to the list of cells in the grid
 
-        public Cell2D(Entity grid, int2 gridCoordinate, int2 worldCoordinate, float2 size, int index, float2 center) {
+        public Cell2D(Entity grid, int3 gridCoordinate, int2 worldCoordinate, float2 size, int index, float2 center) {
             this.grid = grid;
             this.gridCoordinate = gridCoordinate;
             this.worldCoordinate = worldCoordinate;

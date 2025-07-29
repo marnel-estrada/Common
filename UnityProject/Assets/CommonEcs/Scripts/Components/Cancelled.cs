2f8a9a7e481b30854cc36a77d11b2421b5af7da5
marnel.estrada <marnel.estrada@gmail.com>
2025-07-29 09:45:33 +0200

    Generic Cancelled component

---
+++ UnityProject/Assets/CommonEcs/Scripts/Components/Cancelled.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+
+namespace CommonEcs {
+    /// <summary>
+    /// A generic enableable component that marks an entity as "cancelled"
+    /// </summary>
+    public struct Cancelled : IComponentData, IEnableableComponent {    
+    }
+}

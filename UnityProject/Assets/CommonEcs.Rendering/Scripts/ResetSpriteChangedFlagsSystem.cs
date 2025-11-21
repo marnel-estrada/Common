a1af36844502ff9ee81a454bbb405a416939a040
marnel.estrada <marnel.estrada@gmail.com>
2025-11-21 09:51:58 +0100

    Removed usage of Entities.ForEach()

--- UnityProject/Assets/CommonEcs.Rendering/Scripts/ResetSpriteChangedFlagsSystem.cs
+++ UnityProject/Assets/CommonEcs.Rendering/Scripts/ResetSpriteChangedFlagsSystem.cs
@@ -1,3 +1,5 @@
+using Unity.Burst.Intrinsics;
+using Unity.Collections;
 using Unity.Entities;
 
 namespace CommonEcs {
@@ -6,13 +8,36 @@ namespace CommonEcs {
     [UpdateAfter(typeof(SortRenderOrderSystem))]
     [UpdateInGroup(typeof(Rendering2dSystemGroup))]
     public partial class ResetSpriteChangedFlagsSystem : SystemBase {
+        private EntityQuery query;
+
+        protected override void OnCreate() {
+            this.query = new EntityQueryBuilder(Allocator.Temp)
+                .WithAll<Sprite>()
+                .Build(this);
+        }
+
         protected override void OnUpdate() {
-            this.Entities.WithChangeFilter<Sprite>().ForEach((ref Sprite sprite) => {
-                sprite.VerticesChanged = false;
-                sprite.UvChanged = false;
-                sprite.ColorChanged = false;
-                sprite.RenderOrderChanged = false;
-            }).WithBurst().ScheduleParallel();
+            ResetJob resetJob = new() {
+                spriteType = GetComponentTypeHandle<Sprite>()
+            };
+            this.Dependency = resetJob.ScheduleParallel(this.query, this.Dependency);
+        }
+
+        private struct ResetJob : IJobChunk {
+            public ComponentTypeHandle<Sprite> spriteType;
+            
+            public void Execute(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask, in v128 chunkEnabledMask) {
+                NativeArray<Sprite> sprites = chunk.GetNativeArray(ref this.spriteType);
+                
+                DotsAssert.IsFalse(useEnabledMask);
+                for (int i = 0; i < chunk.Count; i++) {
+                    Sprite sprite = sprites[i];
+                    sprite.VerticesChanged = false;
+                    sprite.UvChanged = false;
+                    sprite.ColorChanged = false;
+                    sprite.RenderOrderChanged = false;
+                }
+            }
         }
     }
 }

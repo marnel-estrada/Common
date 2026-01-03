202a76d5e0d5f00a9a4d8502f95793c677982f00
marnel.estrada <marnel.estrada@gmail.com>
2026-01-03 17:25:06 +0100

    Applied rider suggestions

--- UnityProject/Assets/CommonEcs/Scripts/Utils/LinearHashMap/LinearHashMap16.cs
+++ UnityProject/Assets/CommonEcs/Scripts/Utils/LinearHashMap/LinearHashMap16.cs
@@ -158,11 +158,7 @@ namespace CommonEcs {
             return hash & (LinearHashMapBucket16<K, V>.Length - 1);
         }
 
-        public int Count {
-            get {
-                return this.count;
-            }
-        }
+        public int Count => this.count;
 
         public void Clear() {
             for (int i = 0; i < LinearHashMapBucket16<K, V>.Length; ++i) {
@@ -172,10 +168,6 @@ namespace CommonEcs {
             this.count = 0;
         }
 
-        public LinearHashMapBucket16<K, V>.Enumerator Entries {
-            get {
-                return this.bucket.GetEnumerator();
-            }
-        }
+        public LinearHashMapBucket16<K, V>.Enumerator Entries => this.bucket.GetEnumerator();
     }
 }

2018b64f95ab2f3d7eab905446fcb88e8795e607
marnel.estrada <marnel.estrada@gmail.com>
2026-09-04 19:26:44 +0200

    RunInBackground

---
+++ UnityProject/Assets/Common.Components/Scripts/RunInBackground.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Common {
+    /// <summary>
+    /// This is useful for web builds so that the game tries to run in the background even while not in focus.
+    /// Did it as MonoBehaviour so we don't have to insert this line in some arbitrary existing code.
+    /// </summary>
+    public class RunInBackground : MonoBehaviour {
+        [SerializeField]
+        public bool value = true;
+        
+        private void Awake() {
+            Application.runInBackground = value;
+        }
+    }
+}

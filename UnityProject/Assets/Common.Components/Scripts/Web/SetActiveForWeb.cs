123b283336ca00399c3e9c62b4e43def723eba4b
marnel.estrada <marnel.estrada@gmail.com>
2026-09-06 14:52:02 +0200

    SetActiveForWeb

---
+++ UnityProject/Assets/Common.Components/Scripts/Web/SetActiveForWeb.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+using System;
+#endif
+
+namespace Common.Web {
+    public class SetActiveForWeb : MonoBehaviour {
+        [SerializeField]
+        private GameObject[] activateTargets;
+        
+        [SerializeField]
+        private GameObject[] deactivateTargets;
+
+        private void Awake() {
+#if UNITY_WEBGL && !UNITY_EDITOR
+            foreach (GameObject target in this.activateTargets) {
+                target.SetActive(true);
+            }
+
+            foreach (GameObject target in this.deactivateTargets) {
+                target.SetActive(false);
+            }
+#endif
+        }
+    }
+}

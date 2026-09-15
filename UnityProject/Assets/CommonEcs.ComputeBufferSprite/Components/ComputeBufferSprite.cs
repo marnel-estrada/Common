4af3e5be4eb530a27410f70297aaee038b9083e9
marnel.estrada <marnel.estrada@gmail.com>
2026-09-15 09:12:08 +0200

    Removed has transparent content

--- UnityProject/Assets/CommonEcs.ComputeBufferSprite/Components/ComputeBufferSprite.cs
+++ UnityProject/Assets/CommonEcs.ComputeBufferSprite/Components/ComputeBufferSprite.cs
@@ -11,17 +11,14 @@ namespace CommonEcs {
         // This is the ordering of the sprite within its layer
         public int layerOrder;
 
-        // When this is true, the sprite would be part of transparent entries that need to be sorted
-        public readonly bool hasTransparentContent;
-
-        public ComputeBufferSprite(float2 size, Color color, bool hasTransparentContent = false) : this(size, new float2(0.5f, 0.5f), color, hasTransparentContent) {
+        public ComputeBufferSprite(float2 size, Color color) : 
+            this(size, new float2(0.5f, 0.5f), color) {
         }
 
-        public ComputeBufferSprite(float2 size, float2 pivot, Color color, bool hasTransparentContent = false) : this() {
+        public ComputeBufferSprite(float2 size, float2 pivot, Color color) : this() {
             this.size = size;
             this.pivot = pivot;
             this.color = color;
-            this.hasTransparentContent = hasTransparentContent;
         }
 
         public float Width => this.size.x;
